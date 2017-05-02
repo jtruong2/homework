20c0fe853b4d5545fdaac26eb16a56302bbcbb83
jtruong2 <jtruong2@comcast.net>
2017-05-02 09:09:58 -0600

    initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,24 @@
+=begin
+Bad connection
+1. In this exercise, the problem will be solved when the user has a full conversation with the program.
+2. The interface will ask the user for an input and will display a comment based on the input.
+3. The small picture goal here is to display the desired output based on the user input.
+4.Pseudocode:
+  1. The first step of is to display a greeting to the customer
+  2. Then the next step is to stop and ask the user for an input
+      IF the customer does not input a response
+        The program will display "HELLO?!"
+        Program asks for input
+      ELSE IF the customer asks a question with lower case letters
+        The program responds with "I AM HAVING A HARD TIME HEARING YOU."
+        Program asks for input
+      ELSE IF the customer asks a question with all upper case letters
+        The program responds with "No, THIS IS NOT A PET STORE"
+        Program asks for input
+      ELSE IF the customer replys with "GOODBYE!"
+        The program will display "ANYTHING ELSE I CAN HELP WITH?"
+        The program will prompt for another input
+          IF the customer inputs "GOODBYE!"
+            The program will display "THANK YOU FOR CALLING!"
+3. The Program exits
+=end
